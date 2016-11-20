6f61eba9286effe557d138b8bec599b6c569633d
Conanjun <1526840124@qq.com>
2016-11-20 16:38:07 +0800

    add mergesort heapsort bucketsort

--- Sort/sort.c
+++ Sort/sort.c
@@ -52,6 +52,39 @@ void SimpleSelectSort(ElemType A[],int n){
     }
 }
 
+//大顶堆，排序可得升序排列
+void HeapAdjust(ElemType A[],int s,int n){
+    ElemType temp=A[s];
+    int lchild=2*s+1;//索引从0开始的话其左孩子为2s+1
+    while(lchild<n){
+        if(lchild+1<n && A[lchild+1]>A[lchild])
+            lchild++;
+        if(A[lchild]>A[s]){
+            A[s]=A[lchild];
+            s=lchild;
+            lchild=2*s+1;
+        }else{
+            break;
+        }
+        A[s]=temp;
+    }
+
+}
+void BuildHeap(ElemType A[],int n){
+    for(int i=(n-1)/2;i>=0;i--)
+        HeapAdjust(A,i,n);
+}
+void HeapSort(ElemType A[],int n){
+    BuildHeap(A,n);
+    //不断交换最后一个元素与大顶堆堆顶元素可得到升序排列，再调整堆
+    for(int i=n-1;i>0;i--){
+        ElemType temp=A[i];
+        A[i]=A[0];
+        A[0]=temp;
+        HeapAdjust(A,0,i);
+    }
+}
+
 //交换型
 void BubbleSort(ElemType A[],int n){
     //大数往后
@@ -87,7 +120,46 @@ void QuickSortWrapper(ElemType A[],int n){
     QuickSort(A,0,n-1);
 }
 
+//其他类型
+//将A1归并排序至A2,再根据A2修改A1即可
+void Merge(ElemType A[],int s,int m,int t,ElemType Temp[]){
+    int i=s;
+    int j=m+1;
+    int k=0;
+    while(i<=m && j<=t){
+        if(A[i]<=A[j]) Temp[k++]=A[i++];
+        else Temp[k++]=A[j++];
+    }
+    while(i<=m) Temp[k++]=A[i++];
+    while(j<=t) Temp[k++]=A[j++];
+    for(int i=0;i<k;i++)
+        A[s+i]=Temp[i];//重新调整A
+}
+void MergeSort(ElemType A[],int s,int t,ElemType Temp[]){
+    if(s<t){
+        int mid=(s+t)/2;
+        MergeSort(A,s,mid,Temp);
+        MergeSort(A,mid+1,t,Temp);
+        Merge(A,s,mid,t,Temp);
+    }
+}
+void MergeSortWrapper(ElemType A[],int n){
+   //此处需要根据n来动态创建辅助数组
+    ElemType *Temp=(ElemType *)malloc(n*sizeof(ElemType));
+    MergeSort(A,0,n-1,Temp);
+    free(Temp);
+}
 
-
-
+void BucketSort(ElemType A[],int n){
+    ElemType bucket[Maxsize];
+    for(int i=0;i<Maxsize;i++)
+        bucket[i]=0;//-1 表示该桶未放入元素
+    //将各个元素映射到桶
+    for(int i=0;i<n;i++)
+        ++bucket[A[i]];
+    //遍历各个桶，将所有数一次写入A
+    for(int i=0,j=0;i<Maxsize;i++)
+        for(int k=bucket[i];k>0;k--)
+            A[j++]=i;//bucket的下标为数组元素A[j]
+}
 #endif
--- tests/test_Sort.c
+++ tests/test_Sort.c
@@ -10,7 +10,7 @@ typedef void (*FunType)(int A[],int n);
 
 //利用函数指针传参，传人函数指针，方便用一个testSort测试不同的排序算法而不用考虑数组被修改后不能继续排序验证的问题
 void testSortByFunType(FunType pfun){
-    ElemType a[10]={3,1,5,7,2,4,8,8,10,6};
+    ElemType a[10]={3,1,5,7,2,4,8,9,10,6};
     pfun(a,10);
     printArray(a,10);
 }
@@ -26,4 +26,10 @@ void testSort(){
     testSortByFunType(BubbleSort);
     printf("QuickSortWrapper: ");
     testSortByFunType(QuickSortWrapper);
+    printf("HeapSort: ");
+    testSortByFunType(HeapSort);
+    printf("MergeSort: ");
+    testSortByFunType(MergeSortWrapper);
+    printf("BucketSort: ");
+    testSortByFunType(BucketSort);
 }
